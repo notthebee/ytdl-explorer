aeec61134b160f79eda00113e4c771149c9d4ef8
Wolfgang <mail@weirdrescue.pw>
2021-04-02 18:52:10 +0200

    youtube-dl Auto-update, new screenshot, README.md fixes, fancy new .reg file generator
    Alternative file manager functionality is broken for now due to a new registry file generator that I'm using. Feel free to submit a PR if you find out how to fix it

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # youtube-dl and ffmpeg Windows Explorer Integration
 Download videos from YouTube/Twitch/Twitter and more (any platform that is supported by youtube-dl) right in the Windows Explorer, without installing any shady shareware apps!
 
-![Screenshot](https://user-images.githubusercontent.com/30384331/108196593-2ee3dd00-7111-11eb-955b-a2f3c29f58cc.png)
+![Screenshot](res/screenshot.png)
 
-I made this script for myself, since I reference other YouTube videos and memes a lot in my own content and needed an easy way to download videos in an editing-friedly format (DNxHR 25 FPS in my case).
+I made this script for myself, since I reference other YouTube videos and memes a lot in my own content and needed an easy way to download videos in an editing-friedly format (DNxHR 25 FPS in my case). The script will also update youtube-dl automatically if a new version is detected.
 
 ### Supported formats:
 * Audio: MP3, WAV
 * Video: MP4 H.264, MOV DNxHR
 
 ### How to use it
-1. Right-click on "Raw" and save the file
-2. Double-click on the .reg file and confirm adding the keys to the registry
+1. Clone the repository or download it as ZIP
+2. Double-click on the ytdl.reg file and confirm adding the keys to the registry
 3. Copy the video link, go to the folder where you want to download it
 4. Right click on the empty space and choose your option
 5. Voilà!
@@ -25,3 +25,6 @@ Set-ExecutionPolicy Bypass -Scope Process
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install youtube-dl ffmpeg
 ```
+
+### Uninstalling ytdl-explorer
+To uninstall the script, Double-click on the uninstall.reg file and confirm the changes.
---
+++ RegFileGenerator.py
@@ -0,0 +1,98 @@
+from textwrap import dedent
+from string import ascii_lowercase
+
+class RegGenerator():
+    def __init__(self):
+        self.types = {
+            "h264": {'options': '', 'icon': 18, 'subformats':True, 'name': 'Download video (H.264)'},
+            "dnxhr": {'options': '--format=bestvideo+bestaudio --exec=\'ffmpeg -i {} -c:v dnxhd -profile:v dnxhr_hq -vf fps=25/1,format=yuv422p -c:a pcm_s16le {}.mov & del {}\'', 'icon': 41, 'subformats': False, 'name': 'Download video (DNxHR 25 FPS)'},
+            "mp3": {'options': '--continue -f bestaudio -x --audio-format mp3', 'icon': 103, 'subformats': False, 'name': 'Download audio (MP3)'},
+            "wav": {'options': '--continue -f bestaudio -x --audio-format wav', 'icon': 80, 'subformats': False, 'name': 'Download audio (WAV)'},
+            "playlist": {'options': '--yes-playlist -o \'%%(playlist)s/%%(playlist_index)s - %%(title)s.%%(ext)s\' -i --continue --format=bestvideo+bestaudio[ext=m4a]/best --merge-output-format=mp4', 'icon': 97, 'subformats': False, 'name': 'Download playlist (H.264)'},
+            "playlist_mp3": {'options': '--yes-playlist -o \'%%(playlist)s/%%(playlist_index)s - %%(title)s.%%(ext)s\' -i --continue --format=bestaudio -x --audio-format mp3', 'icon': 128, 'subformats': False, 'name': 'Download playlist (MP3)'},
+        }
+        self.reg_common = "HKEY_CLASSES_ROOT\Directory\Background\shell\YoutubeDL"
+        self.command_common = \
+        """
+        @="powershell.exe -Command
+        \\"$latest = Invoke-WebRequest -Uri https://github.com/ytdl-org/youtube-dl/releases/latest -Method get -MaximumRedirection 0 -ErrorAction Ignore;
+        $latest = ($latest.headers.location -split '/')[-1];
+        $current = youtube-dl --version; if($current -ne $latest) { Write-Output 'Youtube-DL needs to be updated. Please grant the administrator privileges in the next dialog. This is a one-time process';
+        Start-Sleep -s 5;
+        start-process powershell.exe '-Command youtube-dl -U' -Verb RunAs -Wait};
+        youtube-dl $(Get-Clipboard) 
+        --continue
+        --no-check-certificate
+        """
+        self.command_common = " ".join(self.command_common.split())
+        self.command_common_end = '\\""'
+        self.start = \
+        """
+        Windows Registry Editor Version 5.00
+        ; Youtube downloader context menu
+        ; by notthebee
+
+        ; Deleting the previous version
+        [-{reg_common}]
+
+
+        [{reg_common}]
+        "MUIVerb"="youtube-dl"
+        "SubCommands"=""
+
+        [{reg_common}\shell]
+
+        """.format(reg_common = self.reg_common)
+        self.start = dedent(self.start)
+
+    def gen_all(self):
+        
+
+        types_string = ""
+        count = 0
+        for type, subtypes in self.types.items():
+            reg_root_dir = "[{reg_common}\shell\{alpha_type}_{type}]".format(reg_common = self.reg_common, alpha_type = ascii_lowercase[count], type = type)
+            if subtypes['subformats']:
+                    at = '"MUIVerb"'
+                    subcommands = '"SubCommands"=""'
+                    reg_root_dir_command = reg_root_dir[:-1] + "\shell]"
+
+                    subformats = \
+                    """
+                    [{rrdc}\\a_best]
+                    @="Best quality"
+
+                    [{rrdc}\\a_best\\command]
+                    {command_common} --format=bestvideo+bestaudio[ext=m4a]/best --merge-output-format=mp4 {command_end}
+
+                    [{rrdc}\\b_1080p]
+                    @="1080p"
+
+                    [{rrdc}\\b_1080p\\command]
+                    {command_common} --format=bestvideo[height<=1080]+bestaudio[ext=m4a]/best --merge-output-format=mp4 -o '%%(title)s_1080.%%(ext)s' {command_end}
+
+                    [{rrdc}\\c_720p]
+                    @="720p"
+
+                    [{rrdc}\\c_720p\\command]
+                    {command_common} --format=bestvideo[height<=720]+bestaudio[ext=m4a]/best --merge-output-format=mp4 -o '%%(title)s_720.%%(ext)s' {command_end}
+                    """.format(rrdc = reg_root_dir_command[1:-1], command_common = self.command_common, command_end = self.command_common_end)
+                    command = dedent(subformats)
+            else:
+                    at = "@"
+                    subcommands = ""
+                    reg_root_dir_command = reg_root_dir[:-1] + "\command]"
+                    command = self.command_common + " " + subtypes['options'] + self.command_common_end
+            reg_name = '{}="{}"'.format(at, subtypes['name'])
+            icon = '"Icon"="imageres.dll,{}"'.format(subtypes['icon'])
+
+            result_string = ("\n").join([reg_root_dir, reg_name, subcommands, icon, reg_root_dir_command, command, ""]).replace("\n\n", "\n")
+            types_string += "\n" + result_string
+            count += 1
+        result = self.start + types_string
+        with open("ytdl.reg", "w+") as regfile:
+            regfile.write(result.strip())
+        
+
+
+RegGenerator().gen_all()
